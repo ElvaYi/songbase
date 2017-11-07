ce9ac73b89128ace8b79b96aa1dfac0d558f2ca2
Elva Yi <ElvaYi@example.com>
2017-11-07 10:31:55 -0500

    added python file

---
+++ songbase.py
@@ -0,0 +1,21 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h1>hello world !!!</h1>'
+
+
+@app.route('/users')
+def get_users(name):
+    return 'hello %s your age is %d' % (name, 3)
+
+
+@app.route('/users')
+def show_all_users(name):
+    return '<h2>this is the page for all users</h2>'
+
+
+if __name__ == '__main__':
+    app.run()
