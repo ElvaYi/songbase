8a44766596abcc852caa972437e26112f2f9dd9c
Elva Yi <ElvaYi@example.com>
2017-11-09 10:56:49 -0500

    add some file

--- songbase.py
+++ songbase.py
@@ -1,15 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1>hello world !!!</h1>'
+    # return '<h1>hello world !!!</h1>'
+    return render_template('index.html')
 
 
 @app.route('/user/<string:name>/')
-def get_users(name):
-    return 'hello %s your age is %d' % (name, 3)
+def get_user(name):
+    # return 'hello %s your age is %d' % (name, 3)
+    return render_template('user.html', user_name=name)
+
+
+@app.route('/songs')
+def get_all_songs():
+    songs = [
+        'song1',
+        'song2',
+        'song3'
+    ]
+    return render_template('songs.html', songs=songs)
+
+
+@app.route('/mysong')
+def mysong():
+    return render_template('mysong.html')
 
 
 @app.route('/users')
