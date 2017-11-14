c0abdff9209d210d7a4c199eb7e0ee7c75cbfc96
Elva Yi <ElvaYi@example.com>
2017-11-14 10:53:41 -0500

    inclass assignment

--- songbase.py
+++ songbase.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'randomtype'
 
 
 @app.route('/')
@@ -8,20 +9,37 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/form-demo', methods=['GET', 'POST'])
+def form_demo():
+    # how to get form data is different for GET vs. POST
+    if request.method == 'GET':
+        first_name = request.args.get('first_name')
+        if first_name:
+            return render_template('form-demo.html', first_name=first_name)
+    else:
+        return render_template('form-demo.html', first_name=session.get('first_name'))
+
+
+if request.method == 'POST':
+        session['first_name'] = request.form['first_name']
+        # return render_template('form-demo.html', first_name=first_name)
+return redirect(url_for('form_demo'))
+
+
 @app.route('/user/<string:name>/')
 def get_user(name):
     # return 'hello %s your age is %d' % (name, 3)
     return render_template('user.html', user_name=name)
 
 
-@app.route('/songs')
+@app.route('/songs-no-extend')
 def get_all_songs():
     songs = [
         'song1',
         'song2',
         'song3'
     ]
-    return render_template('songs.html', songs=songs)
+    return render_template('songs-no-extend.html', songs=songs)
 
 
 @app.route('/mysong')
